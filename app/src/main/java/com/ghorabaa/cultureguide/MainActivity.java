48cb54cf71228a835a34437e92e1661f568c2f47
HaneenWally <megemen2020@gmail.com>
2018-03-26 19:18:04 +0200

    Add Sign Up page

--- app/src/main/java/com/ghorabaa/cultureguide/MainActivity.java
+++ app/src/main/java/com/ghorabaa/cultureguide/MainActivity.java
@@ -1,13 +1,27 @@
 package com.ghorabaa.cultureguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import static android.util.Log.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void launchSignUpActivity(View view) {
+        Intent intent = new Intent(this, activity_signUp.class);
+        startActivity(intent);
+    }
+
 }
