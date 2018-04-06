b2b9e4765d5f8064c7bcb6269be32c6aa74ae625
Ahmed Ibrahim <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2018-04-06 20:50:50 +0200

    Modify organization edit profile

---
+++ app/src/main/java/com/ghorabaa/cultureguide/EditProfile/EditOrgContract.java
@@ -0,0 +1,12 @@
+package com.ghorabaa.cultureguide.EditProfile;
+
+/**
+ * Created by Ahmed Ibrahim on 4/6/18.
+ */
+
+public interface EditOrgContract {
+    public interface EditOrgView{
+        void onSuccess(String SuccessMessage);
+        void onFail(String SuccessMessage);
+    }
+}
