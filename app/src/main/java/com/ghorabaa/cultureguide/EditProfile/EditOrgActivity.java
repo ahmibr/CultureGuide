845000a5ba65102dee24aad7c47775dc335c5d1a
Bassel Mostafa <bassel.ahmed97@eng-st.cu.edu.eg>
2018-04-13 18:53:48 +0200

    Fixed Blank Fields

--- app/src/main/java/com/ghorabaa/cultureguide/EditProfile/EditOrgActivity.java
+++ app/src/main/java/com/ghorabaa/cultureguide/EditProfile/EditOrgActivity.java
@@ -56,12 +56,24 @@ public class EditOrgActivity extends AppCompatActivity
 
     public void changeOrgName(View view){
         String newName = ((TextView) findViewById(R.id.edit_org_name)).getText().toString();
+
+        if(newName.isEmpty()){
+            printToast("Please fill the blank field",Toast.LENGTH_LONG);
+            return;
+        }
+
         showProgressBar("Changing name","Please wait while changing name...");
         mPresenter.changeName( newName );
     }
 
     public void changeOrgEmail(View view){
         String email = ((TextView) findViewById(R.id.edit_org_email)).getText().toString();
+
+        if(email.isEmpty()){
+            printToast("Please fill the blank field",Toast.LENGTH_LONG);
+            return;
+        }
+
         showProgressBar("Changing email","Please wait while changing email...");
         mPresenter.changeEmail( email );
     }
@@ -74,6 +86,12 @@ public class EditOrgActivity extends AppCompatActivity
             printToast("Passwords doesn't match",Toast.LENGTH_LONG);
             return;
         }
+
+        if(password.isEmpty()){
+            printToast("Please fill the blank field",Toast.LENGTH_LONG);
+            return;
+        }
+
         showProgressBar("Changing password","Please wait while changing password...");
         mPresenter.changePassword( password );
     }
