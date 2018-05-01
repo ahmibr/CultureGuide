3f84fa37bb998706caff85d03b522c366b53e1dd
HaneenWally <megemen2020@gmail.com>
2018-05-01 16:02:08 +0200

    Add admin view admins

---
+++ app/src/main/java/com/ghorabaa/cultureguide/AdminViewAdmins/AdminViewAdminContract.java
@@ -0,0 +1,8 @@
+package com.ghorabaa.cultureguide.AdminViewAdmins;
+
+/**
+ * Created by megem on 5/1/2018.
+ */
+
+public interface AdminViewAdminContract {
+}
---
+++ app/src/main/java/com/ghorabaa/cultureguide/AdminViewUser/AdminViewUserActivity.java
@@ -0,0 +1,15 @@
+package com.ghorabaa.cultureguide.AdminViewUser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ghorabaa.cultureguide.R;
+
+public class AdminViewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view_user);
+    }
+}
