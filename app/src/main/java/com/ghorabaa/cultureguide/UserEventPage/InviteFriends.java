0c383ea1180f839aca678b9b0dbd35a37d131bb4
Bassel Mostafa <bassel.ahmed97@eng-st.cu.edu.eg>
2018-05-04 19:36:47 +0200

    Invite Friends front end integration

--- app/src/main/java/com/ghorabaa/cultureguide/UserEventPage/InviteFriends.java
+++ app/src/main/java/com/ghorabaa/cultureguide/UserEventPage/InviteFriends.java
@@ -3,11 +3,13 @@ package com.ghorabaa.cultureguide.UserEventPage;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ghorabaa.cultureguide.Friend;
 import com.ghorabaa.cultureguide.R;
@@ -29,25 +31,53 @@ public class InviteFriends extends AppCompatActivity implements InviteFriendsCon
         mPresenter.retrieveFriendsList();
     }
 
-    public void onRetrieveFriendsList(ArrayList<Friend> friendsList){
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.invite_friend_row, null);
+    public void onRetrieveFriendsList(final ArrayList<Friend> friendsList){
         for (int i = 0; i < friendsList.size() ; i++)
         {
-            rowView = inflater.inflate(R.layout.invite_friend_row, null);
-            mFriendParent.addView(rowView, mFriendParent.getChildCount() - 1);
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.invite_friend_row, null);
 
-            TextView friendName = (TextView) findViewById(R.id.invite_friends_name);
-            friendName.setText(friendsList.get(i).getName());
+            ((TextView)(((LinearLayout)rowView).getChildAt(0))).setText(friendsList.get(i).getName());
 
-            //TODO complete add invite button logic here
-            Button inviteButton = (Button) findViewById(R.id.invite_friends_invite_button);
+            final Button inviteButton = (Button) ((LinearLayout)rowView).getChildAt(1);
             inviteButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    if((LinearLayout)view.getParent() != null)
+                    {
+                        LinearLayout row = (LinearLayout)view.getParent();
+                        LinearLayout rowParent = (LinearLayout)row.getParent();
 
+                        for (int i = 0 ; i < rowParent.getChildCount(); i++){
+                            if(rowParent.getChildAt(i) == row) {
+                                Toast.makeText(view.getContext(), "Invited " + friendsList.get(i).getName() , Toast.LENGTH_SHORT).show();
+                                mPresenter.inviteFriend(i);
+
+                                inviteButton.setEnabled(false);
+                            }
+                        }
+                        return;
+                    }
+
+                    int index = ((LinearLayout)view.getParent()).indexOfChild(view);
+                    Toast.makeText(view.getContext(), index, Toast.LENGTH_LONG).show();
                 }
             });
+
+            //TextView friendName = (TextView) findViewById(R.id.invite_friends_name);
+            //friendName.setText(friendsList.get(i).getName());
+
+            mFriendParent.addView(rowView, mFriendParent.getChildCount());
+
+
+            //TODO complete add invite button logic here
+//            Button inviteButton = (Button) findViewById(R.id.invite_friends_invite_button);
+//            inviteButton.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Toast.makeText(view.getContext(),view.getId() + " test " , Toast.LENGTH_LONG).show();
+//                }
+//            });
         }
     }
 
