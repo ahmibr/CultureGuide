3093f7f24219a4d38c7a94b8f46abf954cff4318
Bassel Mostafa <bassel.ahmed97@eng-st.cu.edu.eg>
2018-05-07 20:53:40 +0200

    Favorites done

--- app/src/main/java/com/ghorabaa/cultureguide/UserSidebar/Favorites/FavoritesActivity.java
+++ app/src/main/java/com/ghorabaa/cultureguide/UserSidebar/Favorites/FavoritesActivity.java
@@ -2,15 +2,23 @@ package com.ghorabaa.cultureguide.UserSidebar.Favorites;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.ghorabaa.cultureguide.R;
 
 import java.util.ArrayList;
 
-public class FavoritesActivity extends AppCompatActivity implements FavoritesContract.View{
+public class FavoritesActivity extends AppCompatActivity implements FavoritesContract.View, AdapterView.OnItemClickListener{
 
     FavoritesContract.Presenter mPresenter;
+
+    private ListView mFavoriteList;
+    private ArrayAdapter<String> mArrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +26,32 @@ public class FavoritesActivity extends AppCompatActivity implements FavoritesCon
 
         mPresenter = new FavoritesPresenter(this,getApplicationContext());
         mPresenter.retrieveFavorites();
+
+        mFavoriteList = (ListView) findViewById(R.id.favorites_list);
+
+        mFavoriteList.setOnItemClickListener(this);
     }
 
 
     @Override
     public void onRetrieveFavorites(ArrayList<String> favoritesList) {
 
+        if(favoritesList.isEmpty())
+        {
+            Toast.makeText(this,"You have no favorites", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String[] favoritesListStrings = new String[favoritesList.size()];
+
         //for testing
-        for(int i=0;i<favoritesList.size();++i)
-            Toast.makeText(getApplicationContext(),favoritesList.get(i),Toast.LENGTH_LONG).show();
+        for(int i=0;i<favoritesList.size();++i){
+            favoritesListStrings[i] = getResources().getString(R.string.remove) + " " + favoritesList.get(i);
+        }
+
+        mArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, favoritesListStrings);
+
+        mFavoriteList.setAdapter(mArrayAdapter);
     }
 
     @Override
@@ -43,5 +68,12 @@ public class FavoritesActivity extends AppCompatActivity implements FavoritesCon
     @Override
     public void onRemoveFail(String errorMessage) {
         Toast.makeText(getApplicationContext(),errorMessage,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        mPresenter.removeFavorite(i);
+
+        mPresenter.retrieveFavorites();
     }
 }
