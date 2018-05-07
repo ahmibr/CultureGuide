aa35a3aa15d0014ef8c7ce8018b58d25bac76c15
Bassel Mostafa <bassel.ahmed97@eng-st.cu.edu.eg>
2018-05-08 00:26:20 +0200

    Added Interests

--- app/src/main/java/com/ghorabaa/cultureguide/UserSidebar/Interests/InterestsActivity.java
+++ app/src/main/java/com/ghorabaa/cultureguide/UserSidebar/Interests/InterestsActivity.java
@@ -2,16 +2,36 @@ package com.ghorabaa.cultureguide.UserSidebar.Interests;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.ghorabaa.cultureguide.R;
 
 import java.util.ArrayList;
 
-public class InterestsActivity extends AppCompatActivity implements InterestsContract.View{
+public class InterestsActivity extends AppCompatActivity implements InterestsContract.View {
 
     InterestsContract.Presenter mPresenter;
+
+    private ListView mInterestListView;
+    private InterestsAdapter mAdapter;
+
+    private ArrayList<Pair<String, Boolean>> mInterestsList = new ArrayList<Pair<String, Boolean>>();
+
+    private int chosenIndex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +39,17 @@ public class InterestsActivity extends AppCompatActivity implements InterestsCon
 
         mPresenter = new InterestsPresenter(this,getApplicationContext());
         mPresenter.retrieveInterests();
+
+        mInterestListView = (ListView) findViewById(R.id.interests_list);
     }
 
     @Override
     public void onRetrieveInterests(ArrayList<Pair<String, Boolean>> interestsList) {
 
-        //testing
-        for(int i=0;i<interestsList.size();++i) {
-            String name = interestsList.get(i).first;
-            String checked = interestsList.get(i).second.toString();
-            Toast.makeText(getApplicationContext(), name + " " + checked,Toast.LENGTH_LONG).show();
-        }
+        mInterestsList = interestsList;
+
+        mAdapter = new InterestsAdapter();
+        mInterestListView.setAdapter(mAdapter);
     }
 
     @Override
@@ -39,7 +59,7 @@ public class InterestsActivity extends AppCompatActivity implements InterestsCon
 
     @Override
     public void onAddSuccess() {
-
+        Toast.makeText(this,"Interest added successfully",Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -49,11 +69,65 @@ public class InterestsActivity extends AppCompatActivity implements InterestsCon
 
     @Override
     public void onRemoveSuccess() {
-
+        Toast.makeText(this,"Interest removed successfully",Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onRemoveFail(String errorMessage) {
 
+    }
+
+    class InterestsAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mInterestsList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return i;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.interest_list_item,null);
+
+            TextView name = (TextView)view.findViewById(R.id.interest_list_name);
+            name.setText( mInterestsList.get(i).first );
+
+            ToggleButton interestedToggle = (ToggleButton) view.findViewById(R.id.interests_toggle);
+
+            interestedToggle.setChecked( mInterestsList.get(i).second );
+
+            interestedToggle.setTag(i);
+
+            interestedToggle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int id = chosenIndex;
+
+                    //Toast.makeText(getApplicationContext(), view.getTag().toString(),Toast.LENGTH_LONG).show();
+
+                    Log.d("Bassel" , view.getTag() + " ");
+
+                    if(((ToggleButton)view).isChecked())
+                    {
+                        mPresenter.addInterest((int)view.getTag());
+                    }
+                    else
+                    {
+                        mPresenter.removeInterest((int)view.getTag());
+                    }
+                }
+            });
+
+            return view;
+        }
     }
 }
